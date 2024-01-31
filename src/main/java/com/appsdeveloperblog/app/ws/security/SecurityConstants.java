42e3f03065bfda30ffd9dc3f5532b52039a73e28
K-Z007 <kevin.zg.tech@gmail.com>
2024-01-31 13:19:56 +1100

    increase Token expiration time from 10 mins to 100 mins

--- src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -7,7 +7,7 @@ import org.springframework.core.env.Environment;
 public class SecurityConstants
 {
     //Token expiration time (normally should be only 1 hour);
-    public static final long EXPIRATION_TIME=600000;  // 11 mins
+    public static final long EXPIRATION_TIME=6000000;  // 100 mins
     public static final String TOKEN_PREFIX="Bearer ";
     public static final String HEADER_STRING="Authorization";
     public static final String SIGN_UP_URL="/users";
